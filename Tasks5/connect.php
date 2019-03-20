e477b0e7569df96cee862fc07c8e2ebafdba17f8
user <user@kab305-17>
2019-03-20 19:53:24 +0200

    1

---
+++ Tasks5/connect.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$pdo = new PDO ('mysql:host=localhost;dbname=test2000;charset=utf8','root','', array (PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+
---
+++ Tasks5/logout.php
@@ -0,0 +1,6 @@
+<?php
+
+require ('connect.php');
+unset($_SESSION['logged_user_id']);
+header('location: index.php');
+
